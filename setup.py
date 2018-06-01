9fcc35c9ff96e002b954ac8250e3932f3e70406d
shashi-banger <shashidhar@amagi.com>
2018-06-01 16:14:02 +0530

    Added schema dependency

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     entry_points={'console_scripts': ['pyinstrument = pyinstrument.__main__:main',
                                       'ffmpeg_cmd_gen=ffmpeg_cmd_gen.ffmpeg_cmd_gen:main']},
     zip_safe=False,
+    install_requires=["schema"],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
